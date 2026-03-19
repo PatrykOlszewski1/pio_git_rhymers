5e3665903e8a44c72125e8fb815493bf0f856521
Mateusz Grabiński <255592@edu.p.lodz.pl>
2026-03-19 21:13:45 +0100

    Poprawki do punktu 6

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,35 +2,36 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int SIZE_OF_NUMBERS = 12;
-    public static final int TOTAL_VALUE = -1;
-    private final int[] numbers = new int[SIZE_OF_NUMBERS];
+    public static final int CAPACITY_OF_NUMBERS = 12;
+    public static final int INITIAL_VALUE = -1;
+    public static final int RETURN_VALUE = -1;
+    private final int[] numbers = new int[CAPACITY_OF_NUMBERS];
 
-    public int total = TOTAL_VALUE;
+    public int total = INITIAL_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-        public boolean callCheck() {
-            return total == TOTAL_VALUE;
-        }
-        
-            public boolean isFull() {
-                return total == 11;
-            }
-        
-                protected int peekaboo() {
-                    if (callCheck())
-                        return TOTAL_VALUE;
-                    return numbers[total];
-                }
-            
-                    public int countOut() {
-                        if (callCheck())
-                            return TOTAL_VALUE;
-                        return numbers[total--];
-                    }
+    public boolean callCheck() {
+        return total == INITIAL_VALUE;
+    }
+
+    public boolean isFull() {
+        return total == CAPACITY_OF_NUMBERS - 1;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return RETURN_VALUE;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return RETURN_VALUE;
+        return numbers[total--];
+    }
 
 }
