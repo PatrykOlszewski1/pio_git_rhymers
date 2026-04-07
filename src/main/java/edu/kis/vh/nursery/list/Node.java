f3b0be04339b473be5276036eeb3271c50f14029
Mateusz Grabiński <255592@edu.p.lodz.pl>
2026-04-07 19:16:41 +0200

    7.1 dodanie final do pola value w klasie node

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    public int value;
+    public final int value;
     public Node prev, next;
 
     public Node(int i) {
@@ -10,3 +10,4 @@ public class Node {
     }
     
 }
+ 
